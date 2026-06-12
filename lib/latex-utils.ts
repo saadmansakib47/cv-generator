7e540f11014baefa4c5a4155126a48132ffda055
saadmansakib47 <saadmansakib@iut-dhaka.edu>
2026-06-12 19:19:43 +0600

    chore : add latex utils

---
+++ lib/latex-utils.ts
@@ -0,0 +1,14 @@
+export function escapeLatex(text: string): string {
+  if (!text) return '';
+  return text
+    .replace(/\\/g, '\\textbackslash{}')
+    .replace(/&/g, '\\&')
+    .replace(/%/g, '\\%')
+    .replace(/\$/g, '\\$')
+    .replace(/#/g, '\\#')
+    .replace(/_/g, '\\_')
+    .replace(/{/g, '\\{')
+    .replace(/}/g, '\\}')
+    .replace(/\^/g, '\\^{}')
+    .replace(/~/g, '\\~{}');
+}
